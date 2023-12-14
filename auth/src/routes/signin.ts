eec100fb2744dab737bff263a1631aa1b4ea735c
Shahnawaz Idariya <shahnawazidariya52@gmail.com>
2023-12-14 00:19:57 -0800

    Applying logic for signin signout middlewares

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,22 +1,45 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator'
 import { RequestValidationError } from '../errors/request-validation-error';
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.post('/api/users/signin',
-[
-    body('email').isEmail().withMessage('Email must be valid'),
-    body('password').trim().notEmpty().withMessage('You must apply a password')
+    [
+        body('email').isEmail().withMessage('Email must be valid'),
+        body('password').trim().notEmpty().withMessage('You must apply a password')
+    ],
+    validateRequest, 
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
 
+        const existingUser = await User.findOne({ email });
+        if(!existingUser) {
+            throw new BadRequestError('Invalid Credentials');
+        }
 
-], 
-(req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
-    res.send('Hi there')
-})
+        const passwordsMatch = await Password.compare(existingUser.password, password);
+        if(!passwordsMatch) {
+            throw new BadRequestError('Invalid Credentials');
+        }
+
+        // Generate JWT
+        const userJwt = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email
+        }, process.env.JWT_KEY!);
+
+        // Store it on session Object
+        req.session = {
+            jwt: userJwt
+        }
+
+        res.status(200).send(existingUser);
+    })
 
 export { router as signinRouter };
