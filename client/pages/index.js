ed5e92dd5170c8735042e80f9488accf9bbfa240
Shahnawaz Idariya <shahnawazidariya52@gmail.com>
2023-12-20 11:51:43 -0800

    cooking reusable Client API

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,16 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
-const LandingPage = ({ currentuser }) => {
-  console.log(currentuser)
-  axios.get('/api/users/currentuser')
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser)
+  // axios.get('/api/users/currentuser')
     return <h1>Landing Page</h1>;
 };
 
-// LandingPage.getInitialProps = async () => {
-//   const response = await axios.get('/api/users/currentuser')
-//   console.log('I am on the server!');
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-//   return response.data;
-// }
+  return data;
+}
    
 export default LandingPage;
