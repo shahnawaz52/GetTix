83082e4c6672a559ea124317acf94f371e56d810
Shahnawaz Idariya <shahnawazidariya52@gmail.com>
2023-12-21 15:42:32 -0800

    publishing npm package @shahntickets/common

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,11 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator'
-import { RequestValidationError } from '../errors/request-validation-error';
-import { validateRequest } from '../middlewares/validate-request';
-import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
-import { Password } from '../services/password';
+import { validateRequest, BadRequestError } from '@shahntickets/common';
 import jwt from 'jsonwebtoken';
+
+import { User } from '../models/user';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest, BadRequestError } from '@shahntickets/common';
 import jwt from 'jsonwebtoken';
-import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
