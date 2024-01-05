8203a805de5d9b0446f0962a575d2d654f212ebd
Shahnawaz Idariya <shahnawazidariya52@gmail.com>
2024-01-05 02:19:57 -0800

    Added order service, added REST APIs for orders and wrote test case

---
+++ orders/src/routes/delete.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
+import { NotAuthorizedError, NotFoundError, OrderStatus } from '@shahntickets/common';
+
+const router = express.Router();
+
+router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if(!order) {
+        throw new NotFoundError();
+    }
+    if(order.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();   
+
+    res.status(204).send(order);
+});
+
+export { router as deleteOrderRouter };
---
+++ orders/src/routes/index.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@shahntickets/common';
+import { body } from 'express-validator';
+import { Order } from '../models/order';
+
+const router = express.Router();
+
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id,
+  }).populate('ticket');
+
+  res.send(orders);
+});
+
+export { router as indexOrderRouter };
