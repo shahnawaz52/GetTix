47074e478a8a59a108fb2704c02a4cacbd4828ec
Shahnawaz Idariya <shahnawazidariya52@gmail.com>
2024-01-05 03:09:27 -0800

    publishing events in order service

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from 'express';
 import { Order } from '../models/order';
 import { NotAuthorizedError, NotFoundError, OrderStatus } from '@shahntickets/common';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
 
     if(!order) {
         throw new NotFoundError();
@@ -16,7 +18,15 @@ router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
         throw new NotAuthorizedError();
     }
     order.status = OrderStatus.Cancelled;
-    await order.save();   
+    await order.save();
+
+    // Publishing an event saying this was cancelled
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+        id: order.id,
+        ticket: {
+            id: order.ticket.id
+        }
+    });
 
     res.status(204).send(order);
 });
